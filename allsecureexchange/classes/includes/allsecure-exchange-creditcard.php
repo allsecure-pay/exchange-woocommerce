461b97da25d37fde8fd1686dad7b8a35e29b3cc3
AllSecure <41504556+allsecure-pay@users.noreply.github.com>
2019-10-22 17:18:48 +0200

    Display generic error message to user on any payment errors

--- allsecureexchange/classes/includes/allsecure-exchange-creditcard.php
+++ allsecureexchange/classes/includes/allsecure-exchange-creditcard.php
@@ -66,7 +66,7 @@ class WC_AllsecureExchange_CreditCard extends WC_Payment_Gateway
         AllsecureExchange\Client\Client::setApiUrl($this->get_option('apiHost'));
         $client = new AllsecureExchange\Client\Client(
             $this->get_option('apiUser'),
-            $this->get_option('apiPassword'),
+            htmlspecialchars_decode($this->get_option('apiPassword')),
             $this->get_option('apiKey'),
             $this->get_option('sharedSecret')
         );
@@ -191,6 +191,7 @@ class WC_AllsecureExchange_CreditCard extends WC_Payment_Gateway
     private function paymentFailedResponse()
     {
         $this->order->update_status('failed', __('Payment failed or was declined', 'woocommerce'));
+        wc_add_notice(__('Payment failed or was declined', 'woocommerce'), 'error');
         return [
             'result' => 'error',
             'redirect' => $this->get_return_url($this->order),
