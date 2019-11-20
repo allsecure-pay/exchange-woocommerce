705e927f5dd525126f1c209d16c85e6b284ec6d1
AllSecure <41504556+allsecure-pay@users.noreply.github.com>
2019-11-20 09:50:39 +0100

    disable individual methods

--- allsecureexchange/classes/includes/allsecure-exchange-provider.php
+++ allsecureexchange/classes/includes/allsecure-exchange-provider.php
@@ -9,14 +9,14 @@ final class WC_AllsecureExchange_Provider
          */
         return [
             'WC_AllsecureExchange_CreditCard',
-            'WC_AllsecureExchange_CreditCard_Amex',
-            'WC_AllsecureExchange_CreditCard_Diners',
-            'WC_AllsecureExchange_CreditCard_Discover',
-            'WC_AllsecureExchange_CreditCard_Jcb',
-            'WC_AllsecureExchange_CreditCard_Maestro',
-            'WC_AllsecureExchange_CreditCard_Mastercard',
-            'WC_AllsecureExchange_CreditCard_UnionPay',
-            'WC_AllsecureExchange_CreditCard_Visa',
+            // 'WC_AllsecureExchange_CreditCard_Amex',
+            // 'WC_AllsecureExchange_CreditCard_Diners',
+            // 'WC_AllsecureExchange_CreditCard_Discover',
+            // 'WC_AllsecureExchange_CreditCard_Jcb',
+            // 'WC_AllsecureExchange_CreditCard_Maestro',
+            // 'WC_AllsecureExchange_CreditCard_Mastercard',
+            // 'WC_AllsecureExchange_CreditCard_UnionPay',
+            // 'WC_AllsecureExchange_CreditCard_Visa',
         ];
     }
 
