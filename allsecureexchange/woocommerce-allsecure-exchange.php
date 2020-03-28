d682f907d40757d4b375930f64b369d89b3d4057
AllSecure <41504556+allsecure-pay@users.noreply.github.com>
2020-03-28 18:44:06 +0100

    Footer Fix

--- allsecureexchange/woocommerce-allsecure-exchange.php
+++ allsecureexchange/woocommerce-allsecure-exchange.php
@@ -62,13 +62,13 @@ add_action('plugins_loaded', function () {
 	}
 	
 	/* Add the gateway footer to WooCommerce */
-	function allsecurefooter() {
+	function allsecureexchange_footer() {
 		$selected_allsecure = new WC_AllsecureExchange_CreditCard;
 		$selectedBanner = $selected_allsecure->get_selected_banner();
 		$selectedCards = $selected_allsecure->get_selected_cards();
 		$selectedBank = $selected_allsecure->get_merchant_bank();
 		if (strpos($selectedCards, 'VISA') !== false) { $visa =  '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/visa.svg">';} else $visa = '';
-		if (strpos($selectedCards, 'MASTER') !== false) { $master = '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/master.svg">';} else $master = '';
+		if (strpos($selectedCards, 'MASTERCARD') !== false) { $mastercard = '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/mastercard.svg">';} else $master = '';
 		if (strpos($selectedCards, 'MAESTRO') !== false) { $maestro = '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/maestro.svg">';} else $maestro = '';
 		if (strpos($selectedCards, 'AMEX') !== false) {$amex = '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/amex.svg">';} else $amex = '';
 		if (strpos($selectedCards, 'DINERS') !== false) {$diners = '<img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/diners.svg">';} else $diners = '';
@@ -84,13 +84,13 @@ add_action('plugins_loaded', function () {
 		$bank = '<a href="'.$bankUrl.'" target="_new" ><img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/'.$selectedBank.'.svg"></a>';
 		$vbv = '<a href="https://rs.visa.com/pay-with-visa/security-and-assistance/protected-everywhere.html" target="_new" ><img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/visa_secure.svg"></a>';
 		$mcsc = '<a href="https://www.mastercard.rs/sr-rs/consumers/identity-check.html" target="_new" ><img src="' . plugin_dir_url( __FILE__ ) . 'assets/images/'.$selectedBanner.'/mc_idcheck.svg"></a>';
-		$allsecure_cards = $visa.''.$master.''.$maestro.''.$diners.''.$amex.''.$jcb ;
+		$allsecure_cards = $visa.''.$mastercard.''.$maestro.''.$diners.''.$amex.''.$jcb ;
 		if ($selectedBanner !== 'none') {
-			$allsecure_banner = '<div id="allsecure_banner"><div class="allsecure">'.$allsecure.'</div><div class="allsecure_threeds">'.$vbv.' '.$mcsc.'</div><div class="allsecure_cards">'.$allsecure_cards.'</div><div class="allsecure_bank">'.$bank.'</div></div>';
-			wp_enqueue_style( 'allsecure_style', plugin_dir_url( __FILE__ ) . 'assets/css/allsecure-style.css', array(), null );
+			$allsecure_banner = '<div id="allsecure_exchange_banner"><div class="allsecure">'.$allsecure.'</div><div class="allsecure_threeds">'.$vbv.' '.$mcsc.'</div><div class="allsecure_cards">'.$allsecure_cards.'</div><div class="allsecure_bank">'.$bank.'</div></div>';
+			wp_enqueue_style( 'allsecure_style', plugin_dir_url( __FILE__ ) . 'assets/css/allsecure-exchange-style.css', array(), null );
 			echo  $allsecure_banner;
 		}
 	}
-	add_filter('wp_footer', 'allsecurefooter'); 
+	add_filter('wp_footer', 'allsecureexchange_footer'); 
 
 });
